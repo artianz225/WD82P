384fcd03018a27fd76e85a22cccc4becbdd3c419
Arthur John Philipps Epiz <artianz225@gmail.com>
2023-08-31 20:51:43 +0800

    add lesson 17 index html

---
+++ 17-JavaScript_Conditionals/main.js
@@ -0,0 +1,65 @@
+console.log(2 > 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+let result = 5 > 4;
+
+console.log(result);
+
+console.log('Z' > 'A');
+
+console.log('Glow' > 'Glee');
+
+console.log('Bee' > 'Be');
+
+console.log('2' > 1);
+
+console.log('01' == 1);
+
+console.log(true == 1);
+
+console.log(false == 0);
+
+console.log('0' === 0);
+
+if (0 > 1) {
+  console.log("1 is indeed larger than 0");
+}
+else {
+  console.log("0 is not greater than 1");
+}
+
+let hour = 15;
+
+if (hour < 12) {
+  console.log("Good Morning");
+} else if (hour < 18) {
+  console.log("Good Afternoon");  
+} else {
+  console.log("Good Evening");
+}
+
+let userLoggedIn = true;
+
+if (userLoggedIn = 0) {
+  console.log("Go to homepage");
+} else {
+  console.log("Rediect to homepage");
+}
+
+let grade =parseInt( prompt ("wthat is your grade?"));
+console.log(grade);
+
+if (grade <= 75) {
+  console.log("Failed! try again quarter");
+} else if (grade <= 79) {
+  console.log("Pass! Exert more effort");  
+} else if (grade <= 84) {
+  console.log("Good keep it up");  
+}  else if (grade <= 89 ) {
+  console.log("Very good. A job well done");  
+}  else {
+  console.log("Execllent");
+}
+
+
